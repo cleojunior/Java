b87ed3c0cca170709e8665032857839cbdae91ff
cleojunior <cleo.junior@gmail.com>
2012-11-07 13:55:14 -0200

    Refreshing Chat project

--- Chat/br/com/chj/SimpleChatClient.java
+++ Chat/br/com/chj/SimpleChatClient.java
@@ -6,10 +6,14 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class SimpleChatClient {
-	JTextArea incoming;
-	JTextField outgoing;
-	BufferedReader reader;
-	PrintWriter writer;
+	private JTextArea incoming;
+	private JTextField outgoing;
+	private BufferedReader reader;
+	private PrintWriter writer;
+	private Socket sock;
+	private static final String LOCAL_HOST = "127.0.0.1";
+	private static final Integer PORT = 5000;
+	
 	
 	public static void main(String[] args) {
 		SimpleChatClient client = new SimpleChatClient();
@@ -34,12 +38,36 @@ public class SimpleChatClient {
 		JButton sendButton = new JButton("Send");
 		sendButton.addActionListener(new SendButtonListener());
 		
-		/* TODO: -> Continuar em pág. 384 Use a Cabeça JAVA.pdf */
+		mainPanel.add(qScroller);
+		mainPanel.add(outgoing);
+		mainPanel.add(sendButton);
+		
+		setUpNetworking();
+		
+		Thread readerThread = new Thread(new IncomingReader());
+		readerThread.start();
+		
+		frame.getContentPane().add(BorderLayout.CENTER, mainPanel);
+		frame.setSize(400, 500);
+		frame.setVisible(true);		
+	}	
+}
+
+private void setUpNetworking() {
+	try {
+		sock = new Socket(LOCAL_HOST, PORT);
+		InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
+		reader = new BufferedReader(streamReader);
+		writer = new PrintWriter(sock.getOutputStream());
+		System.out.println("Networking established");				
+	} catch (Exception ex) {
+		ex.printStackTrace();
 	}
 }
 
 public class SendButtonListener implements ActionListener {
 	public void actionPerformed(ActionEvent ev) {
+		/* TODO: -> Continuar em pág. 385 Use a Cabeça JAVA.pdf */
 		
 	}	
 }
